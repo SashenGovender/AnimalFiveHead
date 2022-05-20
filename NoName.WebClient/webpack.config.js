87b917a9908c490530026b2487ac0c94773f7ca3
Sashen <sashen50@gmail.com>
2022-05-20 23:40:38 +0200

    added npm and tsc inits for web client

---
+++ NoName.WebClient/webpack.config.js
@@ -0,0 +1,26 @@
+//https://webpack.js.org/guides/typescript/
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'wwwroot'),
+    library: 'webClient',
+    libraryTarget: 'umd'
+  },
+};
