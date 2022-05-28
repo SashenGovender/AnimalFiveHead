a2d4e09f9514b692d5e7ebef34379597b9448bd8
SashenGovender <sashen50@gmail.com>
2022-05-28 14:44:49 +0200

    updated user post deploy scripts

--- Database.AnimalFiveHead/Security/NoNameLogin.sql
+++ Database.AnimalFiveHead/Security/NoNameLogin.sql
@@ -1,1 +1,7 @@
-CREATE LOGIN [NoNameLogin] WITH PASSWORD = 'ThisIsALongPassword&50'
+--https://stackoverflow.com/questions/1379437/checking-if-a-sql-server-login-already-exists
+
+IF NOT EXISTS(SELECT * FROM SYS.SYSLOGINS WHERE name='NoNameLogin')
+  BEGIN
+    CREATE LOGIN [NoNameLogin] WITH PASSWORD = 'ThisIsALongPassword&50'
+    PRINT 'NoNameLogin created'
+  END
--- Database.AnimalFiveHead/Security/NoNameUser.sql
+++ Database.AnimalFiveHead/Security/NoNameUser.sql
@@ -1,6 +1,24 @@
-CREATE USER [NoNameUser]
-  FOR LOGIN NoNameLogin
+--CREATE USER [NoNameUser]
+--  FOR LOGIN NoNameLogin
 
-GO
+--GO
+
+--GRANT CONNECT TO [NoNameUser]
+
+
+--https://stackoverflow.com/questions/1601186/sql-server-script-to-create-a-new-user
+
+IF EXISTS(SELECT * FROM SYS.SYSLOGINS WHERE name='NoNameLogin')
+  BEGIN
+    IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = N'NoNameUser')
+      BEGIN
+        CREATE USER [NoNameUser] FOR LOGIN [NoNameLogin]
+        PRINT 'NoNameUser created'
+      END
+  END;
 
 GRANT CONNECT TO [NoNameUser]
+
+ EXEC sp_addrolemember @rolename = N'db_owner',  @loginame = N'NoNameLogin'
+
+GO
